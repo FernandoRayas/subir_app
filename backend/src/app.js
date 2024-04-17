8a5feea05973037d78f0033b323866165dd1af22
Fernando Rayas <138520837+FernandoRayas@users.noreply.github.com>
2024-04-17 00:01:09 -0600

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://login-f758d.web.app/",
+    origin: "https://login-f758d.web.app",
     // preflightContinue: true,
     credentials: true,
   })
